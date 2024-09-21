4d944c57a6ae82b4c601356ee5a98d755ac495ce
Luis Jonson <luislindonjonson@yahoo.br>
2024-09-20 22:51:45 -0300

    adding dada to  react

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ const url = "http://localhost:3000/products"
 
 function App() {
   const [products, setProduct] = useState([])
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState("")
   //1- resgatando dados
-
+  //Carrega a lista
   useEffect(() => {
     async function fetchData() {
       const res = await fetch(url)
@@ -16,7 +18,24 @@ function App() {
     fetchData()
   }, [])
 
-  
+  //2 - add de produtos
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const product = {
+      name,
+      price,
+    }
+    console.log(product)
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(product)
+    })
+  }
+
   return (
     <div className="App">
       <h1>List of products</h1>
@@ -25,6 +44,19 @@ function App() {
           <li key={product.id}>{product.name} - R$: {product.price}</li>
         ))}
       </ul>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input type="text" value={name} name='name' onChange={(e) => setName(e.target.value)} />
+          </label>
+          <label>
+            Price:
+            <input type="number" value={price} name='price' onChange={(e) => setPrice(e.target.value)} />
+          </label>
+          <input type="submit" value="criar" />
+        </form>
+      </div>
     </div>
   )
 }
