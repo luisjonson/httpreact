42b203ea9a9b8eb37de83b8156514d2b6db1ba70
Luis Jonson <luislindonjonson@yahoo.br>
2024-09-20 22:16:01 -0300

    resgatando dados da API(getting data of API)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const url = "http://localhost:3000/products"
 
+function App() {
+  const [products, setProduct] = useState([])
+  //1- resgatando dados
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url)
+      const data = await res.json()
+      setProduct(data)
+    }
+    fetchData()
+  }, [])
+
+  
   return (
     <div className="App">
       <h1>List of products</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name} - R$: {product.price}</li>
+        ))}
+      </ul>
     </div>
   )
 }
